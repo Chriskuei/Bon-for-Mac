39a1fbba77016dcf1a533d86bdb7af8a7ec420ee
Chriskuei <chenjiangui@outlook.com>
2016-05-08 14:43:20 +0800

    Initial Commit

---
+++ Bon/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bon
+//
+//  Created by Chris on 16/5/8.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
